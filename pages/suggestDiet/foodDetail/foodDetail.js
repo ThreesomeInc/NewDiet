5ee2814c4319b2350b0d464b38e397c37a1303d0
martinho <martinho0330@gmail.com>
2018-08-18 17:16:49 +0800

    add hover window

--- pages/suggestDiet/foodDetail/foodDetail.js
+++ pages/suggestDiet/foodDetail/foodDetail.js
@@ -1,24 +1,5 @@
 // pages/foodDetail.js
 const app = getApp();
-const dummy_resp = {
-  "advice": "",
-  "errorCode": 0,
-  "errorMsg": null,
-  "name": "小麦(粒)",
-  "label": ["低嘌呤","低血压"],
-  "preference": "",
-  "composition": {
-    "钠": "6.8克",
-    "水": "10.0克",
-    "碳水化合物": "75.2克",
-    "磷": "325克",
-    "脂肪": "1.3克",
-    "热量": "339千卡",
-    "蛋白质": "11.9克",
-    "钾": "289克"
-  },
-  "dieticianAdvice": "您的肾脏功能属于第一期，血压偏高，该食物蛋白质和纳含量低，可以经常食用。"
-}
 
 
 Page({
@@ -31,8 +12,14 @@ Page({
     foodInfo: {
       imageUrl: 'https://kidneyhealty.com.cn/images/item_icon.png',
     },
-    food_composition:null,
-
+    food_composition: null,
+    showModalStatus: true,
+    animationData: null,
+    preferenceMap: [
+      {key: "1", class: "preference-button", value: "不经常吃"},
+      {key: "2", class: "preference-button", value: "一般"},
+      {key: "3", class: "preference-button", value: "经常吃"},
+    ]
   },
 
   /**
@@ -69,24 +56,93 @@ Page({
           //   foodInfo: dummy_resp,
           // });
           that.setData({
-            food_composition: [                           
-              { name: "蛋白质", value: this.data.foodInfo.composition["蛋白质"] },
-              { name: "热量", value: this.data.foodInfo.composition["热量"] },
-              { name: "碳水化合物", value: this.data.foodInfo.composition["碳水化合物"] },  
-              { name: "脂肪", value: this.data.foodInfo.composition["脂肪"] },
-              { name: "水", value: this.data.foodInfo.composition["水"] },
-              { name: "钠", value: this.data.foodInfo.composition["钠"] },
-              { name: "钾", value: this.data.foodInfo.composition["钾"] },
-              { name: "磷", value: this.data.foodInfo.composition["磷"] },              
+            food_composition: [
+              {name: "蛋白质", value: this.data.foodInfo.composition["蛋白质"]},
+              {name: "热量", value: this.data.foodInfo.composition["热量"]},
+              {name: "碳水化合物", value: this.data.foodInfo.composition["碳水化合物"]},
+              {name: "脂肪", value: this.data.foodInfo.composition["脂肪"]},
+              {name: "水", value: this.data.foodInfo.composition["水"]},
+              {name: "钠", value: this.data.foodInfo.composition["钠"]},
+              {name: "钾", value: this.data.foodInfo.composition["钾"]},
+              {name: "磷", value: this.data.foodInfo.composition["磷"]},
             ]
           });
-          
+
         },
         fail: res => {
           wx.hideLoading();
         }
       });
     }
+  },
+  showModal: function () {
+    // 显示遮罩层
+    let animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    });
+    this.animation = animation;
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    });
+    setTimeout(function () {
+      animation.translateY(0).step();
+      this.setData({
+        animationData: animation.export()
+      });
+    }.bind(this), 200)
+  },
+  hideModal: function () {
+    // 隐藏遮罩层
+    let animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    });
+    this.animation = animation;
+    animation.translateY(300).step();
+    this.setData({
+      animationData: animation.export(),
+    });
+    setTimeout(function () {
+      animation.translateY(0).step();
+      this.setData({
+        animationData: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
+  },
+
+  updatePreference: function (e) {
+    let preference = e.target.dataset.preference;
+    let foodCode = this.data.foodCode;
+    wx.request({
+      url: "http://localhost:8080/food/preference",
+      data: {
+        "userId": app.globalData.authInfo.openid,
+        "foodId": foodCode,
+        "preference": preference,
+      },
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      dataType: "json",
+      success: res => {
+        if (res.data.status === 500) {
+          wx.showModal({
+            title: res.data.message,
+            status: "fail"
+          })
+        }
+        this.hideModal();
+      },
+      fail: res => {
+      }
+    });
   },
 
   /**
