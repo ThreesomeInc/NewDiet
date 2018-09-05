f253635b267f9ade1d6f949166a89e297f005a59
martinho <martinho0330@gmail.com>
2018-09-06 02:02:07 +0800

    add user info

--- pages/threeDayDietReport/threeDayDietReport.js
+++ pages/threeDayDietReport/threeDayDietReport.js
@@ -9,11 +9,11 @@ Page({
     title: {
       headerText: '三日膳食分析',
       subHeader: '您，吃对了吗？',
-    }, 
+    },
     standardLog: null,
     elementEvgs: null,
     nutritionRatio: null,
-    dieticianAdvice:null,
+    dieticianAdvice: null,
     headerMapping: {
       totalEnergy: "总能量",
       totalProtein: "总蛋白质",
@@ -26,17 +26,17 @@ Page({
       k: "钾",
       p: "磷 (建议<800mg/d)",
       ca: "钙 (建议<2000mg/d)"
-    }, 
+    },
     hasResult: false,
-    basicInfoSummary:[],
+    basicInfoSummary: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let logDate = options.logDate
-    console.log(logDate)
+    let logDate = options.logDate;
+    console.log(logDate);
     wx.request({
       url: app.globalData.apiBase + "/foodLog/analysis",
       method: "GET",
@@ -46,44 +46,44 @@ Page({
       },
       success: res => {
         console.log(res.data);
-        var result = res.data;
-        if (result.standardLog !=null){
+        let result = res.data;
+        if (result.standardLog) {
           this.setData({
             standardLog: result.standardLog,
             hasResult: true,
           })
         }
-        if (result.elementEvgs!=null) {
+        if (result.elementEvgs) {
           this.setData({
             elementEvgs: result.elementEvgs,
           });
           let nutritionRatio = Object.entries(this.data.elementEvgs)
             .map(item => {
-              return { name: this.data.headerMapping[item[0]], value: item[1] }
+              return {name: this.data.headerMapping[item[0]], value: item[1]}
             });
           this.setData({
             nutritionRatio: nutritionRatio,
           })
         }
-        if (result.dieticianAdvice!=null) {
+        if (result.dieticianAdvice) {
           this.setData({
             dieticianAdvice: result.dieticianAdvice,
-          })
+          });
           try {
             if (wx.getStorageSync("basicInfoSummary")) {
               this.setData({
                 basicInfoSummary: wx.getStorageSync("basicInfoSummary")
-              })
+              });
               console.log("Session contained basicInfoSummary.");
               console.log(this.data.basicInfoSummary);
-            }else{
+            } else {
               console.log("Session does not contained basicInfoSummary.");
             }
           } catch (e) {
             console.log('Exception happen when try to get basicInfoSummary from storage!');
             console.log(e);
           }
-        }  
+        }
       },
       fail: res => {
         wx.showToast({
@@ -98,48 +98,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
-})
+});
