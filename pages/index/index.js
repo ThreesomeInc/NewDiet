643b9516b1350135155c3c2788a10871a78f4b80
SaraQian <abigail830@163.com>
2018-10-05 13:13:18 +0800

    adding share

--- pages/index/index.js
+++ pages/index/index.js
@@ -46,6 +46,26 @@ Page({
     },
   },
 
+  onShareAppMessage: function (ops) {
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+    }
+    return {
+      title: '肾脏健康营养师',
+      path: '/pages/index/guide/guide',
+      imageUrl: 'https://kidneyhealty.com.cn/images/guide2.jpg',
+      success: function (res) {
+        // 转发成功
+        console.log("转发成功:" + JSON.stringify(res));
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
+  },
+
   onLoad: function () {
     if (app.globalData.logoUrl) {
       this.setData({
--- pages/logFood1/logFood.js
+++ pages/logFood1/logFood.js
@@ -336,8 +336,38 @@ Page({
       mealtime: app.globalData.mealtime,
       openId: app.globalData.authInfo.openid
     });
-    wx.showShareMenu({
-      withShareTicket: true
-    });
-  }
+    // wx.showShareMenu({
+    //   withShareTicket: true
+    // });
+  },
+    onShareAppMessage: function (ops) {
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+    }
+    return {
+      title: '肾脏健康营养师',
+      path: '/pages/index/guide/guide',
+      imageUrl: 'https://kidneyhealty.com.cn/images/guide2.jpg',
+      success: function (res) {
+        // 转发成功
+        console.log("转发成功:" + JSON.stringify(res));
+        // var shareTickets = res.shareTickets;
+        // if (shareTickets.length == 0) {
+        //   return false;
+        // }
+        // //可以获取群组信息
+        // wx.getShareInfo({
+        //   shareTicket: shareTickets[0],
+        //   success: function (res) {
+        //     console.log(res)
+        //   }
+        // })
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
+  },
 });
