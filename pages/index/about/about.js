c6839a0e5ea42eed538299706100ce84c8346f23
SaraQian <abigail830@163.com>
2018-10-06 20:52:26 +0800

    fix git pull merge

--- pages/index/about/about.js
+++ pages/index/about/about.js
@@ -71,7 +71,24 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
-  }
+  onShareAppMessage: function (ops) {
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+    }
+    return {
+      title: '肾脏健康营养师',
+      path: '/pages/index/guide/guide',
+      imageUrl: 'https://kidneyhealty.com.cn/images/guide2.jpg',
+      success: function (res) {
+        // 转发成功
+        console.log("转发成功:" + JSON.stringify(res));
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
+  },
+
 })
